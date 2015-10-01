e7a80db9072ba79953aaf6fd1da45f6059598705
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2015-09-30 20:35:16 -0600

    chore(build): Fix build system

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,10 @@ module.exports = {
         },{
           test: /\.(png|jpg)$/,
           loader: 'url-loader?limit=8192'
-        },
+        },{
+          test: /\.json$/,
+          loader: 'json-loader'
+        }
     ]
   },
   jshint: {
